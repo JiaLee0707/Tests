11a248a78811c8c6bf77eee9114e8397660577ec
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-16 10:01:01 +0900

    초기 커밋.

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4147999-a70e-42b0-ab75-59d117274506")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void T01_더하기()
+        {
+            int a = 5;
+            int b = 5;
+            int c = a + b; // + 1;
+
+            int expected = 10;
+
+            Assert.AreEqual(expected, c, "더하기 실패!!");
+        }
+
+        [TestMethod]
+        public void T02_빼기()
+        {
+            int a = 5;
+            int b = 5;
+            int c = a - b; // + 1;
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, c, "빼기 실패!!");
+        }
+
+        [TestMethod]
+        public void T03_1부터_10까지_더하기() {
+            int sum = 0;
+            for(int i=1; i<=10; i++)
+            {
+                sum += i;
+            }
+            int expected = 55;
+
+            Assert.AreEqual(expected, sum, "더하기 실패..");
+        }
+    }
+}
